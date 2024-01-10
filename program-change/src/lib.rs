6867661684b9b4d5dfd908dba523e256371643c2
Dany Laporte <dany_laporte@hotmail.com>
2024-01-09 22:31:47 -0500

    program change transform cc 03 into program change

--- program-change/src/lib.rs
+++ program-change/src/lib.rs
@@ -1,16 +1,14 @@
 use nih_plug::prelude::*;
-use std::{sync::Arc, time::Instant};
+use std::sync::Arc;
 
 struct ProgramChange {
     params: Arc<ProgramChangeParams>,
-    state: Option<State>,
 }
 
 impl Default for ProgramChange {
     fn default() -> Self {
         Self {
             params: Arc::new(ProgramChangeParams::default()),
-            state: None,
         }
     }
 }
@@ -32,20 +30,6 @@ impl Plugin for ProgramChange {
     type SysExMessage = ();
     type BackgroundTask = ();
 
-    fn deactivate(&mut self) {
-        self.state = None;
-    }
-
-    fn initialize(
-        &mut self,
-        _audio_io_layout: &AudioIOLayout,
-        _buffer_config: &BufferConfig,
-        _context: &mut impl InitContext<Self>,
-    ) -> bool {
-        self.state = None;
-        true
-    }
-
     fn params(&self) -> Arc<dyn Params> {
         self.params.clone()
     }
@@ -56,95 +40,32 @@ impl Plugin for ProgramChange {
         _aux: &mut AuxiliaryBuffers,
         context: &mut impl ProcessContext<Self>,
     ) -> ProcessStatus {
-
-        // resend program change every 4 secs.
-        self.state = self.state.take().filter(|s| s.time.elapsed().as_secs() < 4);
-
         while let Some(event) = context.next_event() {
-            let expected = self.params.snapshot();
-
-            if self.state.as_ref().map_or(true, |s| s.sent != expected) {
-                let channel = expected.channel.saturating_sub(1).clamp(0, 15) as u8;
-    
-                context.send_event(NoteEvent::MidiCC {
-                    timing: 0,
+            context.send_event(match event {
+                NoteEvent::MidiCC {
+                    timing,
                     channel,
-                    cc: 0,
-                    value: expected.msb.clamp(0, 127) as f32 / 127.0,
-                });
-                context.send_event(NoteEvent::MidiCC {
-                    timing: 0,
+                    cc: 03,
+                    value,
+                } => NoteEvent::MidiProgramChange {
+                    timing,
                     channel,
-                    cc: 32,
-                    value: expected.lsb.clamp(0, 127) as f32 / 127.0,
-                });
-                context.send_event(NoteEvent::MidiProgramChange {
-                    timing: 0,
-                    channel,
-                    program: expected.pc.clamp(1, 128) as u8 - 1,
-                });
-
-                self.state = Some(State { sent: expected, time: Instant::now() });
-            }
-
-            context.send_event(event);
+                    program: (value * 127.0) as u8,
+                },
+                e => e,
+            });
         }
 
         ProcessStatus::Normal
     }
-
-    fn reset(&mut self) {
-        self.state = None;
-    }
-}
-
-#[derive(PartialEq)]
-struct SentProgramChange {
-    pc: i32,
-    lsb: i32,
-    msb: i32,
-    channel: i32,
-}
-
-struct State {
-    sent: SentProgramChange,
-    time: Instant,    
 }
 
 #[derive(Params)]
-struct ProgramChangeParams {
-    #[id = "channel"]
-    channel: IntParam,
-
-    #[id = "pc"]
-    pc: IntParam,
-
-    #[id = "msb"]
-    msb: IntParam,
-
-    #[id = "lsb"]
-    lsb: IntParam,
-}
-
-impl ProgramChangeParams {
-    fn snapshot(&self) -> SentProgramChange {
-        SentProgramChange {
-            msb: self.msb.value(),
-            channel: self.channel.value(),
-            pc: self.pc.value(),
-            lsb: self.lsb.value(),
-        }
-    }
-}
+struct ProgramChangeParams {}
 
 impl Default for ProgramChangeParams {
     fn default() -> Self {
-        Self {
-            channel: IntParam::new("channel", 1, IntRange::Linear { min: 1, max: 16 }),
-            msb: IntParam::new("msb", 0, IntRange::Linear { min: 0, max: 127 }),
-            lsb: IntParam::new("lsb", 0, IntRange::Linear { min: 0, max: 127 }),
-            pc: IntParam::new("pc", 1, IntRange::Linear { min: 1, max: 128 }),
-        }
+        Self {}
     }
 }
 
